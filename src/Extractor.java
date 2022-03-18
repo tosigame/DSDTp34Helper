b9ad21ed56581a33eb9bebdd204af687f2d8a7ef
tosigame <tosigame@gmail.com>
2022-03-18 21:25:42 +0100

    Yeeaaaa

---
+++ src/Extractor.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Extractor {
+
+    public static void main(String[] args) throws IOException {
+
+        List<String> file1= extractLines(Path.of("/home/tosigame/LogisimDSD/traces (2)/golden_q3.txt"));
+        List<String> file2= extractLines(Path.of("/home/tosigame/LogisimDSD/traces (2)/trace_q3.txt"));
+        for (int i = 0; i < file1.size(); i++) {
+            //if(!file1.get(i).equals(file2.get(i))) {
+            file1.set(i, file1.get(i).replace("001","0"));
+            file1.set(i, file1.get(i).replace("100","1"));
+            file1.set(i, file1.get(i).replace("010","2"));
+            file1.set(i, file1.get(i).replace("011","3"));
+            file1.set(i, file1.get(i).replace("110","4"));
+
+            file2.set(i, file2.get(i).replace("001","0"));
+            file2.set(i, file2.get(i).replace("100","1"));
+            file2.set(i, file2.get(i).replace("010","2"));
+            file2.set(i, file2.get(i).replace("011","3"));
+            file2.set(i, file2.get(i).replace("110","4"));
+                System.out.println( "golden: "+file2.get(i));
+                System.out.println();
+            //}
+        }
+
+
+    }
+
+    public static List<String> extractLines(Path pathOfFile) throws IOException {
+
+        List<String> lines=new ArrayList<>();
+
+        try(BufferedReader br= new BufferedReader(new FileReader(String.valueOf((pathOfFile))))){
+            String nextLine="";
+
+            while(nextLine!=null) {
+                lines.add(nextLine);
+                nextLine=br.readLine();
+
+            }
+
+        }
+
+    return lines;
+    }
+
+}
